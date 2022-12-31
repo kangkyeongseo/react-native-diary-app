4b87c23b21ba0b6e481e991ac3457f6665f6d2cb
kangkyeongseo <kks_big@naver.com>
2022-12-31 18:44:47 +0900

    Home Screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,22 +1,40 @@
 import React from "react";
 import styled from "styled-components/native";
+import colors from "../colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const View = styled.View`
   flex: 1;
+  padding: 0px 50px;
+  padding-top: 100px;
+  background-color: ${colors.bgColor};
+`;
+const Title = styled.Text`
+  color: ${colors.textColor};
+  font-size: 38px;
+  margin-bottom: 100px;
+`;
+
+const Btn = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
   justify-content: center;
   align-items: center;
+  background-color: ${colors.btnColor};
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
 `;
-const Text = styled.Text``;
-const Btn = styled.TouchableOpacity``;
-const BtnText = styled.Text``;
 
 const Home = ({ navigation: { navigate } }) => {
   return (
     <View>
+      <Title>My Journal</Title>
       <Btn onPress={() => navigate("Write")}>
-        <BtnText>Write</BtnText>
+        <Ionicons name="add" color="white" size={40} />
       </Btn>
-      <Text>Home</Text>
     </View>
   );
 };
